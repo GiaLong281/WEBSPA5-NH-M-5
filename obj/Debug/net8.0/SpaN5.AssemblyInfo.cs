dcfeab986a57acef8af2a0bd2bba62e11a73e4c0
GiaLong281 <longngtb01597@gmail.com>
2026-03-29 00:41:10 +0700

    xem code

--- obj/Debug/net8.0/SpaN5.AssemblyInfo.cs
+++ obj/Debug/net8.0/SpaN5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaN5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256ee3594e66c0c944776b912d451418bd987ef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff86e27e8b6b55d28455cb389ebb2b5c66570d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaN5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaN5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
